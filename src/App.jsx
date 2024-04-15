9fe5dba1bd6b966dba5efd3277b2e8a3f6ddcb2d
Fernanda <fecoimbra96@gmail.com>
2024-04-15 18:21:02 -0300

    feat: finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ export function App() {
       id: 4,
       petImg: luna,
       petName: "Luna",
-      petSex: "Femea",
+      petSex: "femea",
       petCity: "Leopoldina",
       petState: "MG"
     }
